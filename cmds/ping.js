afee47e2aa3c71cdce09c224ed3f82841db40d07
desktop1660sup <mahfuzislam.1001@gmail.com>
2026-04-18 04:17:05 +0600

    Show actual latency in ping command

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,6 +1,9 @@
 module.exports = {
   name: "ping",
   run: async (client, message, args, prefix) => {
-    return message.reply(`Pong! 🏓`);
+    const msg = await message.reply("Pinging... 🏓");
+    const ping = msg.createdTimestamp - message.createdTimestamp;
+
+    return msg.edit(`Pong! 🏓\n**Bot Latency:** \`${ping}ms\`\n**API Latency:** \`${Math.round(client.ws.ping)}ms\``);
   }
 };
